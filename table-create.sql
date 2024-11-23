fbfac3185ddfabe993c145243cc5085f0b475144
Lovebird Connoisseur <90262313+Lovebird-Connoisseur@users.noreply.github.com>
2024-11-23 10:47:05 +0000

    add table-create.sql

---
+++ table-create.sql
@@ -0,0 +1,77 @@
+CREATE TABLE Album(
+title TEXT,
+url TEXT,
+PRIMARY KEY (title, url)
+);
+
+CREATE TABLE Category(
+title text PRIMARY KEY
+);
+
+CREATE TABLE Song(
+title TEXT,
+url TEXT,
+releaseDate TEXT,
+lyrics TEXT,
+pageViews INTEGER,
+PRIMARY KEY (title, url)
+);
+
+CREATE TABLE Tag(
+title text PRIMARY KEY
+);
+
+CREATE TABLE Person(
+title text PRIMARY KEY
+);
+
+CREATE TABLE Job(
+title text PRIMARY KEY
+);
+
+CREATE TABLE IsTaggedAs(
+songTitle TEXT,
+songUrl TEXT,
+tagTitle TEXT,
+
+FOREIGN KEY(songTitle) REFERENCES Song(title),
+FOREIGN KEY(songUrl) REFERENCES Song(url),
+FOREIGN KEY(tagTitle) REFERENCES Tag(title),
+PRIMARY KEY (songTitle, songURL, tagTitle)
+);
+
+CREATE TABLE IsFeaturedIn(
+songTitle TEXT,
+songUrl TEXT,
+albumTitle TEXT,
+track INTEGER,
+
+FOREIGN KEY(songTitle) REFERENCES Song(title),
+FOREIGN KEY(songUrl) REFERENCES Song(url),
+FOREIGN KEY(albumTitle) REFERENCES Album(title),
+PRIMARY KEY (songTitle, songUrl, albumTitle)
+);
+
+CREATE TABLE IsCategorizedAs(
+songTitle TEXT,
+songUrl TEXT,
+categoryName TEXT,
+
+FOREIGN KEY(songTitle) REFERENCES Song(title),
+FOREIGN KEY(songUrl) REFERENCES Song(url),
+FOREIGN KEY(categoryName) REFERENCES Category(title),
+PRIMARY KEY (songTitle, songUrl, categoryName)
+);
+
+CREATE TABLE Worked(
+songTitle TEXT,
+songUrl TEXT,
+staffName TEXT,
+jobTitle TEXT,
+
+FOREIGN KEY(songTitle) REFERENCES Song(title),
+FOREIGN KEY(songUrl) REFERENCES Song(url),
+FOREIGN KEY(staffName) REFERENCES Person(title),
+FOREIGN KEY(jobTitle) REFERENCES Job(title),
+PRIMARY KEY (songTitle, songUrl, staffName, jobTitle)
+);
